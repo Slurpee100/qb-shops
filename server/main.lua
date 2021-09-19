e8b2b579a5ce2d66377e2e882f529c63eb87ce33
Slurpee100 <87210266+Slurpee100@users.noreply.github.com>
2021-09-19 15:55:38 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -29,3 +29,106 @@ QBCore.Functions.CreateCallback('qb-shops:server:getLicenseStatus', function(sou
         cb(false)
     end
 end)
+
+RegisterServerEvent('Slurpee:Getitem')
+AddEventHandler('Slurpee:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.AddItem("slurpee", 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["slurpee"], "add")
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+
+
+end)
+
+RegisterServerEvent('Chip:Getitem')
+AddEventHandler('Chip:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.AddItem("dorito_chips", 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["dorito_chips"], "add")
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+
+
+end)
+
+RegisterServerEvent('Coke:Getitem')
+AddEventHandler('Coke:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.AddItem("2litercoke", 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["2litercoke"], "add")
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+
+
+end)
+
+RegisterServerEvent('Sprite:Getitem')
+AddEventHandler('Sprite:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.AddItem("sprite", 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["sprite"], "add")
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+
+
+end)
+
+RegisterServerEvent('Donut:Getitem')
+AddEventHandler('Donut:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.AddItem("donut", 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["donut"], "add")
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+
+
+end)
+
+RegisterServerEvent('Stand:Getitem')
+AddEventHandler('Stand:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.AddItem("hotdog", 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["hotdog"], "add")
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+
+
+end)
+
+RegisterServerEvent('Stand2:Getitem')
+AddEventHandler('Stand2:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+
+    Player.Functions.AddItem("burger-bleeder", 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["burger-bleeder"], "add")
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+
+
+end)
+
+
+RegisterServerEvent('SmallCoke:Getitem')
+AddEventHandler('SmallCoke:Getitem', function(listKey)
+    local src = source 
+    local Player = QBCore.Functions.GetPlayer(src)
+    local random = math.random(1,10)
+    print(random)
+    if random <= 5 then
+        Player.Functions.AddItem("kurkakola", 1)
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["kurkakola"], "add")
+        Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+   else
+    Player.Functions.AddItem("cokebaggy", 1)
+    Player.Functions.RemoveMoney('cash', 10, "cash-removed"..Player.PlayerData.citizenid)
+   end
+
+
+end)
